06746209ac7570c1af0b59c214d03f194cc7bb64
torlin.ia <ilia.torlin@domru.ru>
2019-12-09 20:43:56 +0500

    add Calculator test

--- api/tests/unit/CalculatorTest.php
+++ api/tests/unit/CalculatorTest.php
@@ -9,7 +9,7 @@ use Codeception\Test\Unit;
 
 class CalculatorTest extends Unit
 {
-    public function testBasicCalculatorActions()
+    public function testCalculatorActions()
     {
         $first = new StringLongDigit('6');
         $second = new StringLongDigit('3');
@@ -17,5 +17,24 @@ class CalculatorTest extends Unit
         $this->assertEquals(new LongDigit(1, 1, [9]), $calculator->add());
         $this->assertEquals(new LongDigit(1, 1, [3]), $calculator->sub());
         $this->assertEquals(new LongDigit(1, 2, [1, 8]), $calculator->mult());
+    }
+
+    public function testBasicCalculatorActions()
+    {
+        $first = new StringLongDigit('3.1234567890');
+        $second = new StringLongDigit('3');
+        $calculator = new Calculator([$first, $second]);
+        $this->assertEquals(new LongDigit(1, 1, [6, 1, 2, 3, 4, 5, 6, 7, 8, 9]), $calculator->add());
+        $this->assertEquals(new LongDigit(1, 0, [1, 2, 3, 4, 5, 6, 7, 8, 9]), $calculator->sub());
+        $this->assertEquals(new LongDigit(1, 1, [9, 3, 7, 0, 3, 7, 0, 3, 6, 7]), $calculator->mult());
+    }
+    public function testZeroCalculatorActions()
+    {
+        $first = new StringLongDigit('12345678987654321.98765432123456789');
+        $second = new StringLongDigit('0');
+        $calculator = new Calculator([$first, $second]);
+        $this->assertEquals(new StringLongDigit('12345678987654321.98765432123456789'), $calculator->add());
+        $this->assertEquals(new StringLongDigit('12345678987654321.98765432123456789'), $calculator->sub());
+        $this->assertEquals(new LongDigit(1, 1, [0]), $calculator->mult());
     }
 }
